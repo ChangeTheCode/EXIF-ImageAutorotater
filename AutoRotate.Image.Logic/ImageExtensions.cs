8b57ae775726e75615f7f849fd3a653b2ff362e0
Jim <jim.frey@students.fhv.at>
2019-01-27 16:00:22 +0100

    Refactor method to return the rotation of the picture

--- AutoRotate.Image.Logic/ImageExtensions.cs
+++ AutoRotate.Image.Logic/ImageExtensions.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Drawing;
+using System.IO;
+using System.Windows.Media.Imaging;
 
 namespace AutoRotate.Image.Logic
 {
@@ -11,7 +13,31 @@ namespace AutoRotate.Image.Logic
     {
         private const int exifOrientationID = 0x112; //274
 
-        public static void ExifRotate(this Bitmap img)
+        public static Rotation GetRotation(Bitmap img)
+        {
+            // if this image contains exif information then figure out which case it is
+            if (img.PropertyIdList.Contains(exifOrientationID))
+            {
+                var prop = img.GetPropertyItem(exifOrientationID);
+                int val = BitConverter.ToUInt16(prop.Value, 0);
+                
+                if (val == 3 || val == 4)
+                {
+                    return Rotation.Rotate180;
+                }
+                else if (val == 5 || val == 6)
+                {
+                    return Rotation.Rotate90;
+                }
+                else if (val == 7 || val == 8)
+                {
+                    return Rotation.Rotate270;
+                }
+            }
+            return Rotation.Rotate0;
+        }
+
+        public static void ExifRotate(this System.Drawing.Image img)
         {
             if (!img.PropertyIdList.Contains(exifOrientationID))
                 return;
@@ -33,6 +59,5 @@ namespace AutoRotate.Image.Logic
             if (rot != RotateFlipType.RotateNoneFlipNone)
                 img.RotateFlip(rot);
         }
-
     }
 }
