24035a42de1e1cc5f6f6946f166ffd445b381295
Jim <jim.frey@students.fhv.at>
2019-01-27 16:01:47 +0100

    add configuration class - not included in tool

---
+++ AutoRotate.Image.Console/Configuration/Configuration.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoRotate.Image.Console
+{
+    internal class Configuration : IConfigurationManager
+    {
+
+        /// <summary>
+        /// set config, if key is not in file, create
+        /// </summary>
+        /// <param name="key">Nome do parâmetro</param>
+        /// <param name="value">Valor do parâmetro</param>
+        public void SetConfig(string key, string value)
+        {
+            var configFile = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+            var settings = configFile.AppSettings.Settings;
+            if (settings[key] == null)
+            {
+                settings.Add(key, value);
+            }
+            else
+            {
+                settings[key].Value = value;
+            }
+            configFile.Save(ConfigurationSaveMode.Modified);
+            ConfigurationManager.RefreshSection(configFile.AppSettings.SectionInformation.Name);
+        }
+
+        /// <summary>
+        /// Get key value, if not found, return null
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns>null if key is not found, else string with value</returns>
+        public string GetConfig(string key)
+        {
+            if(string.IsNullOrWhiteSpace(ConfigurationManager.AppSettings[key])){
+                throw new ArgumentException("App setting values is not set of the key", key); 
+            }
+            return ConfigurationManager.AppSettings[key];
+        }
+    }
+}
---
+++ AutoRotate.Image.Console/Configuration/IConfigurationManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AutoRotate.Image.Console
+{
+    internal interface IConfigurationManager
+    {
+        void SetConfig(string key, string value);
+
+        string GetConfig(string key);
+    }
+}
