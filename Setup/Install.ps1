ea8e773b4d0c79a0678b67c24cce7a452c0195a5
ChangeTheCode <ChangeTheCode@users.noreply.github.com>
2021-04-16 15:06:57 +0200

    feat: provide install and remove script for regex entry  (#6)

---
+++ Setup/Install.ps1
@@ -0,0 +1,32 @@
+﻿# this powershell script set the rotation application to the registry.
+# so you can execute the application by navigate to a directory and use the context menu to start the application
+
+# Parameter help description
+Param(
+    [Parameter(Mandatory=$True)]
+    [string]$path
+)
+
+#Store the current working location
+Push-Location
+
+#Set HKEY_CLASSES_ROOT as ps drive 
+New-PSDrive -PSProvider registry -Root HKEY_CLASSES_ROOT -Name HKCR
+
+#Change the current working location to the appropriate registry drive 
+Set-Location HKCR:
+
+# add app to context menu of directories
+New-Item –Path "HKCR:\Directory\shell" –Name "RotationTool"
+
+# add icon for app 
+New-ItemProperty –Path "HKCR:\Directory\shell\RotationTool" –Name "icon" -Value "C:\Source_Private\Github\EXIF-ImageAutorotater\icon.ico"
+
+# create command directory 
+New-Item –Path "HKCR:\Directory\shell\RotationTool" –Name "command"
+
+# add path to the application exe
+Set-Itemproperty –Path "HKCR:\Directory\shell\RotationTool\command" –Name "(Default)" -Value $path
+
+# return to the starting working location.
+Pop-Location
---
+++ Setup/Remove.ps1
@@ -0,0 +1,16 @@
+﻿# this powershell script remove the rotation application from the registry.
+
+#Store the current working location
+Push-Location
+
+#Set HKEY_CLASSES_ROOT as ps drive 
+New-PSDrive -PSProvider registry -Root HKEY_CLASSES_ROOT -Name HKCR
+
+#Change the current working location to the appropriate registry drive 
+Set-Location HKCR:
+
+# remove registry entry 
+Remove-Item -Path "HKCR:\Directory\shell\RotationTool" -Recurse
+
+# return to the starting working location.
+Pop-Location
